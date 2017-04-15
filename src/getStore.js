265549bfb3d6adaaa160bd4c7cc88b191b92fab9
Daniel Stern <daniel@danielstern.ca>
2017-04-15 15:26:12 -0400

    Immutability

--- src/getStore.js
+++ src/getStore.js
@@ -2,6 +2,16 @@ import {
     createStore
 } from 'redux';
 
+import {
+    fromJS
+} from 'immutable';
+
+import {
+    getDefaultState
+} from './../server/getDefaultState';
+
+const defaultState = fromJS(getDefaultState());
+const reducer = state=>state;
 const store = createStore(
     a=>a,
 );
